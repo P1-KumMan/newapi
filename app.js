b3284be22b1ca3eafaa978e38f60f4a8004c843d
P1-KumMan <adithan92014@gmail.com>
2020-11-20 08:23:18 +0530

    added cores

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 require("dotenv/config");
+const cors = require("cors");
 
+app.use(cors());
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
